b4e4f53e8640d1b33abd2256327775c1c7f0de81
allen-byn <nobodyplay@yeah.net>
2021-08-25 22:49:41 +0800

    将需求重新实现了。并且将所有选定的DOM对象放到另一个文件中去，从而节省了main.js中的代码。

---
+++ src/scripts/allObjects.js
@@ -0,0 +1,13 @@
+const elements = new Map();
+
+// add values
+elements.set('wrap', document.querySelector('.waka'));
+elements.set('logo', document.querySelector('svg'));
+elements.set('w', document.querySelector('.w'));
+elements.set('a2', document.querySelector('.a1'));
+elements.set('k', document.querySelector('.k'));
+elements.set('a1', document.querySelector('.a2'));
+elements.set('thumb', document.querySelector('.finger'));
+elements.set('characters', document.querySelector('.character'));
+
+export {elements};
